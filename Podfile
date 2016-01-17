da52de37a1416c81ed4fb01536264b2fc14eecd5
Koji Ota <afterjnih@gmail.com>
2016-01-17 23:41:58 +0900

    use server

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :osx, '10.10'
+use_frameworks!
+
+pod 'PromiseKit', '3.0.1'
+pod 'SwiftyJSON'
--- Tangerine/CenterContainerViewController.swift
+++ Tangerine/CenterContainerViewController.swift
@@ -8,22 +8,24 @@
 
 import Cocoa
 import Quartz
+import PromiseKit
+import SwiftyJSON
 class CenterContainerViewController: NSViewController, WindowControllerDelegate {
-
+	
 	@IBOutlet weak var contentView: PDFContentView!
 	var pdf: PDFDocument!
 	var windowController: WindowController!
 	override var acceptsFirstResponder: Bool { return true }
-  override func viewDidLoad() {
-      super.viewDidLoad()
-    	let PDFViewChangedPageNotification = "PDFViewChangedPage"
-  		let notificationHandler = NSNotificationCenter.defaultCenter()
-		  notificationHandler.addObserver(
-				self,
-				selector: "getPageNotification:",
-				name: PDFViewChangedPageNotification,
-				object: contentView)
-  }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let PDFViewChangedPageNotification = "PDFViewChangedPage"
+		let notificationHandler = NSNotificationCenter.defaultCenter()
+		notificationHandler.addObserver(
+			self,
+			selector: "getPageNotification:",
+			name: PDFViewChangedPageNotification,
+			object: contentView)
+	}
 	override var representedObject: AnyObject? {
 		didSet {
 			// Update the view, if already loaded.
@@ -32,13 +34,19 @@ class CenterContainerViewController: NSViewController, WindowControllerDelegate 
 	override func loadView() {
 		super.loadView()
 	}
-  override func viewWillAppear() {
+	override func viewWillAppear() {
 		print("centerContainerViewController viewWillAppear!")
 		windowController = self.view.window!.windowController! as? WindowController
 		windowController.delegate = self
 		if let doc = windowController.document as? Document {
-  		pdf = PDFDocument(URL: doc.url)
-  		self.contentView.setDocument(pdf)
+			pdf = PDFDocument(URL: doc.url)
+//			self.contentView.setDocument(pdf)
+			let fileName = "hogehoge"
+			MostRecentPageNum(fileName).then({pageN -> () in
+				print(pageN)
+  			self.contentView.setDocument(self.pdf)
+				self.contentView.goToPage(self.pdf.pageAtIndex(pageN))
+			})
 		}
 	}
 	override func viewDidAppear() {
@@ -46,16 +54,33 @@ class CenterContainerViewController: NSViewController, WindowControllerDelegate 
 	}
 	func getPageNotification(notification: NSNotification) {
 		if let windowCt = windowController {
-  		windowCt.updatePageLabel(contentView.currentPageLabel())
+			windowCt.updatePageLabel(contentView.currentPageLabel())
 		}
 	}
 	func whichPage(pageLabel: String) {
 		if let doc = windowController.document as? Document {
 			if let labelPage = doc.labelPage {
 				if let label = labelPage[pageLabel] {
-    			self.contentView.goToPage(pdf.pageAtIndex(label))
+					self.contentView.goToPage(pdf.pageAtIndex(label))
 				}
 			}
+		}
+	}
+	func MostRecentPageNum(fileName: String) -> Promise<Int> {
+		return Promise { fulfill, reject in
+			NSURLSession.GET("http://localhost:3000").then({ response -> Int in
+				do {
+  				let json = JSON(try NSJSONSerialization.JSONObjectWithData(response, options: NSJSONReadingOptions.AllowFragments))
+  				let pageNum = json["page"].int!
+					return pageNum
+				} catch {
+					return 0
+				}
+			}).then({ value -> Void in
+				fulfill(value)
+			}).error({ error -> Void in
+				print("error")
+			})
 		}
 	}
 }
